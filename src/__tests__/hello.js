77772c277b3c94e57a6fb3c2017c5b946b98828d
Julien Wajsberg <felash@gmail.com>
2019-01-16 15:07:40 +0100

    Testcase for the MouseEvent

--- src/__tests__/hello.js
+++ src/__tests__/hello.js
@@ -5,6 +5,21 @@ import Hello from "../hello";
 
 test("It properly renders the component", () => {
   const { container } = render(<Hello />);
+  fireEvent.mouseMove(container.querySelector('.playground'), {
+    clientX: 5,
+    clientY: 10,
+  });
+
+  expect(container.firstChild).toMatchSnapshot();
+
+  fireEvent.mouseMove(container.querySelector('.playground'), {
+    clientX: 5,
+    clientY: 10,
+    pageX: 5,
+    pageX: 10,
+    offsetX: 2,
+    offsetY: 7,
+  });
 
   expect(container.firstChild).toMatchSnapshot();
 });
--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { stripIndent } from 'common-tags';
 
 export default class Hello extends React.PureComponent {
+  state = { log: "" };
+  onMouseMove = (e) => {
+    this.setState({
+      log: stripIndent`
+        clientX: ${e.clientX}
+        clientY: ${e.clientY}
+        pageX: ${e.pageX}
+        pageY: ${e.pageY}
+        x: ${e.nativeEvent.x}
+        y: ${e.nativeEvent.y}
+        offsetX: ${e.nativeEvent.offsetX}
+        offsetY: ${e.nativeEvent.offsetY}
+      `
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div
+        className="playground"
+        style={{width: "500px", height: "500px", background: "lightgreen", whiteSpace: "pre"}}
+        onMouseMove={this.onMouseMove}>
+        {this.state.log}
       </div>
     );
   }
